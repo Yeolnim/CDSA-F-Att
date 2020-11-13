94ba45d2b13d909c4b3d61b44d4aaba748749164
Yeolnim <49664111+Yeolnim@users.noreply.github.com>
2020-11-13 11:08:36 +0800

    Create prepare_dataset.py

---
+++ utils/prepare_dataset.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.utils import shuffle
+import text_util
+
+pos1 = text_util.load_txt("..\raw_data\books\review_negative")
+pos2 = text_util.load_txt("..\raw_data\books\review_positive")
+pos=[]
+file_object=pos1+pos2
+
+try:
+     all_the_text = file_object.read( )
+finally:
+     file_object.close( )
+r = all_the_text.split('\n')
+r=list(r)
+print(r)
+result =pd.DataFrame(columns={'comment','label'})
+for i in range(len(r)):
+        if i<3000:
+           result=result.append({'comment':r[i],'label':1},ignore_index=True)
+        else:
+           result = result.append({'comment': r[i], 'label': 0}, ignore_index=True)
+# print(result)
+# result=shuffle(result)
+print(result)
+result.to_csv("..\data\books.csv",encoding='utf-8')
